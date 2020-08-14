c9836af8c14e6477c316b56e6054474e5a4a71c8
Sabith Pkc Mnr <sabithpkcmnr@gmail.com>
2020-08-15 02:08:38 +0530

    Improved codes

---
+++ app/src/main/java/com/sabithpkcmnr/onesignal/ActivityHome.java
@@ -0,0 +1,15 @@
+package com.sabithpkcmnr.onesignal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        
+    }
+}
